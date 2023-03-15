139c860faa075bafe0c0472a25ddde60f193a26a
oluwasegun31 <oluwasegun1412@gmail.com>
2023-03-15 06:38:13 +0100

    FEAT: added react-router

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,12 @@
-import homeimg1 from '../images/2.png'
-import homeimg2 from '../images/5.png'
-import homeimg3 from '../images/6.png'
-import homeimg4 from '../images/25.png'
-import homeimg5 from '../images/150.png'
-import {FaArrowDown} from 'react-icons/fa'
+import homeimg1 from '../images/2.png';
+import homeimg2 from '../images/5.png';
+import homeimg3 from '../images/6.png';
+import homeimg4 from '../images/25.png';
+import homeimg5 from '../images/150.png';
+import {FaArrowDown} from 'react-icons/fa';
+
 function Home(){
+
     return(
         <section className='home'>
             <div className="images">
@@ -15,7 +17,7 @@ function Home(){
                 <img src={homeimg5} alt="pokeimg" />
             </div>
             <h1>WELCOME TO POKÉ</h1>
-            <p>A site where you can search for your favourite pokemon characters.</p>
+            <p>A site where you search for all of your favourite pokemon characters.</p>
             <div className="arrow-down">
                 <FaArrowDown />
             </div>
