47cbe44ac85833b5db26db48b6288200247c9f8f
oluwasegun31 <oluwasegun1412@gmail.com>
2023-03-20 18:34:19 +0100

    FEAT: template for about page added

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,23 @@
-import AnimatedPage from "./animationPage"
+import AnimatedPage from "./animationPage";
+import {FaGithub} from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
+import {FaInstagram} from 'react-icons/fa';
 
 function About(){
     return(
         <AnimatedPage>
             <section className="about">
-                <h1>COMING SOON...</h1>
+                <article>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores repudiandae tempore necessitatibus officia vero aspernatur eaque alias eveniet veniam dolor animi provident, quae voluptatibus, enim impedit a inventore quidem numquam. Ut sed voluptatibus ducimus quis, dolores impedit fugiat neque obcaecati optio voluptate quam minima. Ratione consequatur amet laboriosam laborum rem, optio ipsam, corporis minima eum harum quod inventore quia numquam distinctio dicta, iste obcaecati. Reiciendis totam laboriosam ex eius vero pariatur odio facilis cum corrupti officiis dolor, architecto deserunt aliquam ea aspernatur alias dicta iusto corporis sunt ipsam quasi fugiat doloribus laudantium. Qui aperiam architecto dolor sed, consectetur iste impedit quae quia doloremque totam. Fugit aspernatur asperiores dolore, saepe iste, quisquam molestias doloremque obcaecati corrupti magni, neque adipisci voluptatum? Architecto dolorum fugiat praesentium asperiores rem libero rerum eligendi quam necessitatibus iusto excepturi maxime, autem alias in vitae. Voluptas rerum debitis corporis reiciendis deserunt cum iste suscipit fuga expedita, molestias maxime facilis accusamus. Non, assumenda eligendi officia iusto quos atque ut a voluptates dolorem qui minima similique nostrum optio! Necessitatibus, aliquid iste provident ipsa rem ducimus voluptatem itaque odio officiis pariatur omnis libero illum incidunt exercitationem aspernatur voluptates consequatur error veritatis obcaecati. Tempore iusto doloribus aspernatur sit aliquam, cumque, fugiat iure unde assumenda ut quisquam deserunt consequuntur odit soluta eos facere omnis laudantium! Nemo dolorum explicabo unde nostrum minima nisi praesentium aperiam libero labore. Sit, accusantium dignissimos, quibusdam aspernatur, fugit accusamus sint quae enim iusto non corporis quas hic exercitationem deserunt officiis incidunt deleniti animi a dolor. Nobis debitis ratione quod aspernatur modi dolore repellendus optio quibusdam voluptatum! Magnam voluptatibus nemo ex nihil temporibus ipsam atque quaerat odio unde, vero suscipit incidunt odit esse magni nostrum aut minima facilis iure eveniet. Iste dolorem provident porro, quisquam corrupti beatae temporibus magni repellendus quos mollitia.
+                </article>
+                <div className="socials">
+                    <p>CONTACTS</p>
+                    <div className="social-icons">
+                        <a href="https://github.com/oluwasegun31"><FaGithub /></a>
+                        <a href="https://twitter.com/__oluwasegun"><FaTwitter /></a>
+                        <a href="https://instagram.com/_oluwa.segun"><FaInstagram /></a>
+                    </div>
+                </div>
             </section>
         </AnimatedPage>
     )
