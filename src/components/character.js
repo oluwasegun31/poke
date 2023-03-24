ccec8243f58c1fb859740647512404a24ff6e27b
oluwasegun31 <oluwasegun1412@gmail.com>
2023-03-24 12:17:43 +0100

    Refactor: Added custom hooks

--- src/components/character.js
+++ src/components/character.js
@@ -1,24 +1,13 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+import useFetch from "../hooks/useFetch";
 import AnimatedPage from "./animationPage";
 import ErrorTemp from "./error";
 import Loader from "./loader";
 
 function Character(){
     const [inputNum, setInputNum] = useState(1);
-    const [submitNum, setSubmitNum] = useState(inputNum);
-    const [frontDef, setFrontDef] = useState("");
-    const [frontShin, setFrontShin] = useState("");
-    const [backDef, setBackDef] = useState("");
-    const [backShin, setBackShin] = useState("");
-    const [pokeName, setPokeName] = useState("");
-    const [pokeHeight, setPokeHeight] = useState("");
-    const [pokeWeight, setPokeWeight] = useState("");
-    const [pokeBaseExp, setBaseExp] = useState("");
-    const [pokeAbility, setPokeAbility] = useState(null);
-    const [pokeStats, setPokeStats] = useState(null);
-
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(null);
+    const [submitNum, setSubmitNum] = useState(inputNum); 
+    const {frontDef, frontShin, backDef, backShin, pokeName, pokeHeight, pokeWeight, pokeBaseExp, pokeAbility, pokeStats, isLoading, setIsLoading, isError} = useFetch(`https://pokeapi.co/api/v2/pokemon/${submitNum}`)
 
     const submitHandle = (e)=>{
         e.preventDefault()
@@ -26,38 +15,7 @@ function Character(){
         setIsLoading(true)
     }
 
-    useEffect(()=>{
-       setTimeout(() => {
-            fetch(`https://pokeapi.co/api/v2/pokemon/${submitNum}`)
-                .then(res=>{
-                    if(!res.ok){
-                        throw Error('Unable to load your reequest')
-                    }
-                    return res.json()
-                })
-                .then(data=>{
-                    console.log(data)
-                    setFrontDef(data.sprites.front_default)
-                    setFrontShin(data.sprites.front_shiny)
-                    setBackDef(data.sprites.back_default)
-                    setBackShin(data.sprites.back_shiny)
-                    setPokeName(data.name)
-                    setPokeHeight(data.height)
-                    setPokeWeight(data.weight)
-                    setBaseExp(data.base_experience)
-                    setPokeAbility(data.abilities)
-                    setPokeStats(data.stats)
-                    
-                    setIsLoading(false)
-                    setIsError(null)
-                })
-                .catch(err=>{
-                    console.log(err.message)
-                    setIsError(err.message)
-                    setIsLoading(false)
-                })
-       }, 5000);
-    }, [submitNum])
+    
 
     return(
         <AnimatedPage >
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+function useFetch(url){
+    const [frontDef, setFrontDef] = useState("");
+    const [frontShin, setFrontShin] = useState("");
+    const [backDef, setBackDef] = useState("");
+    const [backShin, setBackShin] = useState("");
+    const [pokeName, setPokeName] = useState("");
+    const [pokeHeight, setPokeHeight] = useState("");
+    const [pokeWeight, setPokeWeight] = useState("");
+    const [pokeBaseExp, setBaseExp] = useState("");
+    const [pokeAbility, setPokeAbility] = useState(null);
+    const [pokeStats, setPokeStats] = useState(null);
+
+    const [isLoading, setIsLoading] = useState(true);
+    const [isError, setIsError] = useState(null);
+
+    useEffect(()=>{
+        setTimeout(() => {
+             fetch(url)
+                 .then(res=>{
+                     if(!res.ok){
+                         throw Error('Unable to load your reequest')
+                     }
+                     return res.json()
+                 })
+                 .then(data=>{
+                     console.log(data)
+                     setFrontDef(data.sprites.front_default)
+                     setFrontShin(data.sprites.front_shiny)
+                     setBackDef(data.sprites.back_default)
+                     setBackShin(data.sprites.back_shiny)
+                     setPokeName(data.name)
+                     setPokeHeight(data.height)
+                     setPokeWeight(data.weight)
+                     setBaseExp(data.base_experience)
+                     setPokeAbility(data.abilities)
+                     setPokeStats(data.stats)
+                     
+                     setIsLoading(false)
+                     setIsError(null)
+                 })
+                 .catch(err=>{
+                     console.log(err.message)
+                     setIsError(err.message)
+                     setIsLoading(false)
+                 })
+        }, 5000);
+    }, [url])
+    return(
+        {frontDef, frontShin, backDef, backShin, pokeName, pokeHeight, pokeWeight, pokeBaseExp, pokeAbility, pokeStats, isLoading, setIsLoading, isError}
+    )
+}
+
+export default useFetch
